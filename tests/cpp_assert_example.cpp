cdbae192e1e496f8ba295b7acd6184bae929a584
Trevor Swarm <tswarm@lccc.wy.edu>
2024-09-30 17:58:34 +0000

    Updated cpp unit test example

--- tests/cpp_assert_example.cpp
+++ tests/cpp_assert_example.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cassert>
 
+using namespace std;
+
 // Function to calculate the Fibonacci sequence
 int fibonacci(int n) {
     if (n <= 0) {
         return 0;
-    } else if (n == 1) {
+    } else if (n == 5) {
         return 1;
     } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);   
+        return fibonacci(n - 1) + fibonacci(n - 2); 
 
     }
 }
